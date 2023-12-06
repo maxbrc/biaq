060e6ea16d780564f6a6fb624c37ee2a59d49199
Max Bruch <max@biaq.de>
2023-12-06 15:24:44 +0100

    Import basic code
    The existing files are imported

---
+++ app.js
@@ -0,0 +1,34 @@
+let sponsorBoss = {
+  'max': false,
+  'philipp': false,
+  'constantin': false,
+  'aaron': false,
+  'lennard': false,
+}
+
+const sponsoren = document.querySelectorAll(".sponsor");
+
+for (let sponsor of sponsoren) {
+  sponsor.addEventListener('click', function() {
+    let sponsorName = sponsor.classList[1];
+    let img = sponsor.getElementsByTagName("img")[0]
+    if (sponsorBoss[sponsorName]) {
+      img.src = "img/" + sponsorName + ".jpg" //string template literals won't work for some reason
+    } else {
+      img.src = "img/" + sponsorName + "_boss.jpg"
+    }
+    sponsorBoss[sponsorName] = !sponsorBoss[sponsorName]
+  })
+}
+
+
+
+function glowLoop() {
+  setTimeout(() => {
+    sponsoren[0].classList.toggle("glow");
+    sponsoren[1].classList.toggle("glow");
+    glowLoop();
+}, 2000)
+}
+
+glowLoop();
